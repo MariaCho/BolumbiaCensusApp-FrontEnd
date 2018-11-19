ae06a29b523ce9a0fc3a773781ae9e6a45bff5c8
Diego Giraldo <diegoagg95@hotmail.com>
2018-11-19 16:50:05 -0500

    Url fixed

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'dgiral58.auth0.com',
   clientId: '1G3fzBGHKV8LEVz5Ymga9r5FwQDGCgsX',
-  callbackUrl: 'https://35.199.93.75/callback'
+  callbackUrl: 'http://35.199.93.75/callback'
 }
